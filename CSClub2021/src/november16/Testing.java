413cee062b719796a32eb420b3d5a8f7c29b2a2e
samho <samho@LAPTOP-U6S44RL2.attlocal.net>
2021-11-23 19:52:57 -0600

    Added code from 11/16/21.

---
+++ CSClub2021/src/november16/Testing.java
@@ -0,0 +1,19 @@
+package november16;
+
+import java.util.*;
+
+public class Testing {
+
+	public static void main(String[] args) {
+//		System.out.println(2 / 3);
+//		System.out.println(Integer.parseInt("123  "));
+//		System.out.println("hi23r%".replaceAll("[0-9]", ""));
+		
+		int[] arr = {7,8,9};
+		System.out.println(Arrays.toString(arr));
+		
+		char[][] grid = {{'a', 'b'}, {'c','d'}};
+		System.out.println(Arrays.deepToString(grid));
+	}
+	
+}
