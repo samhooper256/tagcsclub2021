1e2657cf8b5f63da8d676bd1f015f8b3e5e06a73
samho <samho@10.203.31.140>
2022-01-25 08:42:15 -0600

    Added january18 package.

---
+++ CSClub2021/src/janurary18/Coding.java
@@ -0,0 +1,27 @@
+package janurary18;
+
+import java.util.ArrayList;
+
+public class Coding {
+
+	public static void main(String[] args) {
+//		System.out.println(3 % 5);
+//		System.out.println(3 % -5);
+//		System.out.println(-3 % 5);
+//		System.out.println(-3 % -5);
+//		int i = 4;
+//		
+//		System.out.println(i++);
+//		
+//		System.out.println(++i);
+		
+		System.out.println(-37 >> 2);
+		System.out.println(-37 >>> 2);
+		
+//		System.out.printf("%.1f, %d", 2.4, 77);
+		
+		ArrayList<String> arrayList;
+		
+	}
+	
+}
