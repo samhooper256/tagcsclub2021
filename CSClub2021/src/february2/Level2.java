1156a48d04dbf2ea6b0375209836e1579ee0b598
samho <samho@10.203.31.140>
2022-02-02 15:20:58 -0600

    Added Level2 and Level3 solutions (2/1/22)

---
+++ CSClub2021/src/february2/Level2.java
@@ -0,0 +1,33 @@
+package february2;
+
+import java.util.Scanner;
+
+public class Level2 {
+
+	public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String name = in.next();
+        double d = in.nextDouble();
+        
+        //Approach 1: Basic if/else.
+        if("circle".equals(name))
+            System.out.printf("%.2f", Math.PI * d * d);
+        else if("square".equals(name))
+            System.out.printf("%.2f", d * d);
+        else if("triangle".equals(name))
+            System.out.printf("%.2f", d * d / Math.sqrt(3));
+        
+        /*
+         
+        //Approach 2: Switch expressions (Java 14+). Note that HackerRank is Java 15.
+        System.out.printf("%.2f", switch(name) {
+            case "circle" -> Math.PI * d * d;
+            case "square" -> d * d;
+            case "triangle" -> d * d / Math.sqrt(3);
+            default -> throw new IllegalStateException(); //we know this will never happen, but compiler makes us write a "default" case.
+        });
+        
+        */
+    }
+	
+}
---
+++ CSClub2021/src/february2/Level3.java
@@ -0,0 +1,47 @@
+package february2;
+
+import java.util.*;
+
+public class Level3 {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        List<Team> teams = new ArrayList<>();
+        
+        while(in.hasNextLine()) {
+            String line = in.nextLine();
+            int i = line.lastIndexOf(' ');
+            int score = Integer.parseInt(line.substring(i + 1));
+            String name = line.substring(0, i);
+            teams.add(new Team(name, score));
+        }
+        
+        Team h = Collections.max(teams);
+        Team l = Collections.min(teams);
+        
+        System.out.printf("Highest: %s (%d)%n", h.name, h.score);
+        System.out.printf("Lowest: %s (%d)%n", l.name, l.score);
+    }
+    
+}
+
+class Team implements Comparable<Team> {
+    
+    String name;
+    int score;
+    
+    Team(String name, int score) {
+        this.name = name;
+        this.score = score;
+    }
+    
+    @Override
+    public int compareTo(Team o) {
+         if(this.score < o.score)
+             return -1;
+        if(this.score == o.score)
+            return 0;
+        return 1;
+    }
+    
+}
