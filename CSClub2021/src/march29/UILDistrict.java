e6bd8be6a08c3874df05fc43b17bc423e6e52404
samho <samho@10.203.1.211>
2022-03-30 10:48:30 -0500

    Added 3/29/22 code.

--- CSClub2021/src/march29/UILDistrict.java
+++ CSClub2021/src/march29/UILDistrict.java
@@ -1,5 +1,25 @@
 package march29;
 
+import java.util.Random;
+
 public class UILDistrict {
 
+	public static void main(String[] args) {
+//		System.out.println(Math.signum(0.0));
+//		System.out.println(Math.signum(-0.0));
+//		System.out.println(0.0 == -0.0);
+//		System.out.println(Math.signum(Double.NaN));
+//		System.out.println(1.0 / 0);
+//		System.out.println(0.0 / 0.0);
+//		System.out.println(0 / 0);
+		
+		System.out.println(Double.MIN_VALUE);
+		System.out.println(Double.MAX_VALUE);
+		
+		Random r = new Random();
+		
+		int x = r.nextInt();
+		int y = r.nextInt(7);
+	}
+	
 }
